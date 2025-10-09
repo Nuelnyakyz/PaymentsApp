8d03f425a3f4520147ac12169c58518afbc39a64
Nuelnyakyz <nuelnyakyz@gmail.com>
2025-10-09 17:15:22 +0300

    added api route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaymentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Mirror API initiate endpoint under web routes as well
+Route::post('/api/payments/initiate', [PaymentController::class, 'initiate']);
+
