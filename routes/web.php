933f7aeaa08739d57764cbea0bdc1778201bcd65
Nuelnyakyz <nuelnyakyz@gmail.com>
2025-10-09 17:21:17 +0300

    removed api rout from routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Mirror API initiate endpoint under web routes as well
-Route::post('/api/payments/initiate', [PaymentController::class, 'initiate']);
 
